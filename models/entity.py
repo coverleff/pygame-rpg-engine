cc22e70f0c84e4c5b430f5c6a5317040ebb40318
coverleff <102760048+coverleff@users.noreply.github.com>
2022-06-16 17:07:02 +0200

    entity collision box initializable

--- models/entity.py
+++ models/entity.py
@@ -4,14 +4,15 @@ import setup
 
 class Entity(models.collisionobject.CollisionObject, models.spriteobject.SpriteObject):
     
-    def __init__(self, position : pg.Vector2, sprite : pg.sprite.Sprite, **kwargs):
-        super().__init__(position = position, collision_rect = pg.Rect(0,0, 15, 15), sprite = sprite, **kwargs)
+    def __init__(self, position : pg.Vector2, sprite : pg.sprite.Sprite, collision_rect : pg.Rect = pg.Rect(0,0, 16, 10), **kwargs):
+        super().__init__(position = position, collision_rect = collision_rect, sprite = sprite, **kwargs)
         self.localcollision_rect.bottom = self.sprite_rect.bottom
         self.localcollision_rect.centerx = self.sprite_rect.centerx
 
     def update_tick(self):
         super().update_tick()
-
+        #self.blit_collisionbox()
+    
     def blit_collisionbox(self):
         collision_surface = pg.Surface(self.collision_rect.size)
         collision_surface.fill(pg.Color(0, 64, 64, 64))
